f4b22a5e387c504f8ff80291aea8c78ff8efc294
Timm Weber <s211273@student.dhbw-mannheim.de>
2023-12-09 11:47:26 -0500

    Added comments, fixed some bugs

--- scanner.py
+++ scanner.py
@@ -1,15 +1,17 @@
 import os
 
-def get_PID_by_files():
+def get_PID_by_files(max_pid):
 
     existing_pids = []
 
     for pid in range(1, max_pid+1):
         try:
+            # If this file exists, then the *thread* exists
             with open(f"/proc/{pid}/status") as file:
                 for line in file:
                     if line.startswith("Tgid"):
-                        tgid = line[line.rindex("\t") + 1:-1] # Remove tab and newline
+                        tgid = line[line.rindex("\t") + 1:-1] # Remove tab and newline to get the TGID
+                        # If the TGID is different from the PID of the current folder then it is only a thread, therefore not shown in the directory listing of /proc
                         if tgid == pid:
                             existing_pids.append(pid)
         except FileNotFoundError:
@@ -20,15 +22,18 @@ def get_PID_by_files():
 def get_PID_by_dir():
     displayed_pids = []
     for file in os.listdir("/proc"):
+        # Filter out the files and folders under /proc that do not correspond to PIDs
         if os.path.isdir(f"/proc/{file}") and file.isnumeric():
             displayed_pids.append(file)
     
     return displayed_pids
 
 def check_hidden_modules():
+
     modules_proc = []
     hidden = []
 
+    # First, get all modules according to /proc/modules (Which is used my lsmod)
     with open("/proc/modules", "r") as file:
 
         for line in file:
@@ -37,6 +42,7 @@ def check_hidden_modules():
 
     modules_sys = os.listdir("/sys/module")
 
+    # Not all subdirectories in /sys/module correspond to modules. But those that contain a sections-subdirectory do.
     for module in modules_sys:
         if "sections" not in os.listdir(f"/sys/module/{module}"):
             modules_sys.remove(module)
@@ -56,23 +62,32 @@ def check_hidden_pid():
     with open("/proc/sys/kernel/pid_max", "r") as file:
         max_pid = int(file.readline())
 
-    existing_pids = set(get_PID_by_files())
-    displayed_pids = set(get_PID_by_dir())
+    # It is possible that some process terminates between get_PID_by_files and get_PID_by_dir
+    # It is also possible that a new process is created inbetween, but that is not a problem, 
+    # as rootkits will probably not hide /proc/<pid>/status without hiding /proc/<pid>
+    existing_pids = set(get_PID_by_files(max_pid))
+    displayed_pids = get_PID_by_dir()
 
-    # With this, it is possible that a PID is listed as a dir under /proc but is terminated before this line, 
-    # so that displayed_pids contains a PID that existing_pids does not contain.
-    # This is fine however, as we can assume that a rootkit would not hide the /proc/<pid>/status file without hiding the directory.
-    existing_pids = existing_pids & set(get_PID_by_files())
+    # With this, only PIDs that have existed before calling get_PID_by_dir as well as after calling it will be taken into consideration
+    existing_pids = existing_pids & set(get_PID_by_files(max_pid))
 
     for pid in existing_pids:
         if pid not in displayed_pids:
             hidden.append(pid)
 
-if __name__ == "main":
-    print("test")
+    return hidden
+
+if __name__ == "__main__":
+    
+    is_detected = False
+
     for module in check_hidden_modules():
+        is_detected = True
         print(f"The module {module} is hidden. It might be a rootkit.")
 
     for pid in check_hidden_pid():
-        print(f"Process {pid} is being hidden. Potential Rootkit-Activity")
+        is_detected = True
+        print(f"Process {pid} is hidden. Potential Rootkit-Activity")
     
+    if not is_detected:
+        print("No signs of rootkits found. This does not mean that there are none.")
